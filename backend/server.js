02cbeace8cf62c244a905a0ad6a086141f2a8c24
coderXdevelop <nandantheunknown.504@gmail.com>
2026-05-06 20:59:24 +0530

    Updated backend cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "https://your-frontend.vercel.app",
+  origin: "https://expense-tracker-u7ud.vercel.app",
   credentials: true,
 }));
 app.use(express.json());
